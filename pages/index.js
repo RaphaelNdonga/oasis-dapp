f88a09fffedf9baa25111e61f1c396ea35f356bd
RaphaelNdonga <nkibi53@gmail.com>
2022-10-24 12:34:56 +0300

    last offer id obtained

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import OffersTable from './components/OffersTable'
 import Web3 from 'web3'
 import { useEffect, useState } from 'react'
+import OasisAddress from "./blockchain/OasisAddress.json";
+import OasisABI from "./blockchain/OasisABI.json";
 
 export default function Home() {
 
   const [connected, setConnected] = useState(false);
   const [signer, setSigner] = useState(null);
   const [web3, setWeb3] = useState(null);
+  const [oasisContract, setOasisContract] = useState(null);
 
   async function connectMetamask() {
     if (window.ethereum) {
@@ -18,34 +20,32 @@ export default function Home() {
     }
   }
 
-  const checkConnection = (accounts) => {
+  const checkConnection = async (accounts) => {
     console.log('checking accounts...', accounts);
     console.log(accounts[0])
     if (accounts[0] === null || accounts[0] === "" || accounts[0] === undefined) {
       console.log("Setting connected to false");
       setConnected(false);
-      localStorage.setItem("metamask", accounts[0]);
+      localStorage.removeItem("metamask");
       setSigner(null);
       setWeb3(null);
     } else {
       console.log("Setting connected to true");
       setConnected(true);
-      localStorage.removeItem("metamask");
+      localStorage.setItem("metamask", accounts[0]);
       setSigner(accounts[0]);
       const _web3 = new Web3(window.ethereum);
       setWeb3(_web3);
+      const _oasisContract = new _web3.eth.Contract(OasisABI, OasisAddress);
+      setOasisContract(_oasisContract);
+      const lastOfferId = await _oasisContract.methods.last_offer_id().call();
+      console.log(lastOfferId);
     }
   }
 
   useEffect(() => {
     const account = localStorage.getItem("metamask");
-    if (account !== undefined) {
-      console.log("accounts 0", account)
-      setSigner(account);
-      setConnected(true);
-    } else {
-      setConnected(false);
-    }
+    checkConnection([account]);
 
     window.ethereum.on("accountsChanged", checkConnection);
 
@@ -53,7 +53,6 @@ export default function Home() {
       window.ethereum.removeListener("accountsChanged", checkConnection);
     }
   }, [])
-
 
   return (
     <>
@@ -73,7 +72,25 @@ export default function Home() {
             </h1>
           </div>
           <div className='flex item-center justify-center p-6 pt-0'>
-            <OffersTable web3={web3} />
+            <table className='table-auto border-collapse border border-slate-400'>
+              <thead>
+                <tr>
+                  <th className='border border-slate-300 p-6'>Buy</th>
+                  <th className='border border-slate-300 p-6'>Price</th>
+                  <th className='border border-slate-300 p-6'>Sell</th>
+                  <th className='border border-slate-300 p-6'>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className='border border-slate-300 p-6'>x</td>
+                  <td className='border border-slate-300 p-6'>10</td>
+                  <td className='border border-slate-300 p-6'>y</td>
+                  <td className='border border-slate-300 p-6'>10</td>
+                </tr>
+
+              </tbody>
+            </table>
           </div>
         </div>
       </main>
